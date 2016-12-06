db0851dcd5206419b3d3dd0f586852c30e640df4
mattcoates <mc955@cam.ac.uk>
2016-12-06 15:24:43 +0000

    EXT setup

--- firmware/jarvis/main.c
+++ firmware/jarvis/main.c
@@ -20,12 +20,44 @@ static THD_FUNCTION(HBT, arg) {
     }
 }
 
+/* EXT Interrupt Config */
+static const EXTConfig extcfg = {
+  {
+    {EXT_CH_MODE_FALLING_EDGE | EXT_CH_MODE_AUTOSTART | EXT_MODE_GPIOB, NULL}, /* PB0 - Radio IRQ */
+    {EXT_CH_MODE_DISABLED, NULL}, /* Px1 */
+    {EXT_CH_MODE_DISABLED, NULL}, /* Px2 */
+    {EXT_CH_MODE_DISABLED, NULL}, /* Px3 */
+    {EXT_CH_MODE_DISABLED, NULL}, /* Px4 */
+    {EXT_CH_MODE_DISABLED, NULL}, /* Px5 */
+    {EXT_CH_MODE_DISABLED, NULL}, /* Px6 */
+    {EXT_CH_MODE_DISABLED, NULL}, /* Px7 */
+    {EXT_CH_MODE_DISABLED, NULL}, /* Px8 */
+    {EXT_CH_MODE_DISABLED, NULL}, /* Px9 */
+    {EXT_CH_MODE_DISABLED, NULL}, /* Px10 */
+    {EXT_CH_MODE_DISABLED, NULL}, /* Px11 */
+    {EXT_CH_MODE_DISABLED, NULL}, /* Px12 */
+    {EXT_CH_MODE_FALLING_EDGE | EXT_CH_MODE_AUTOSTART | EXT_MODE_GPIOB, NULL}, /* PB13 - MPU9250 IRQ */
+    {EXT_CH_MODE_DISABLED, NULL}, /* Px14 */
+    {EXT_CH_MODE_DISABLED, NULL}, /* Px15 */
+    {EXT_CH_MODE_DISABLED, NULL}, /* PVD */
+    {EXT_CH_MODE_DISABLED, NULL}, /* RTC Alarm Event */
+    {EXT_CH_MODE_DISABLED, NULL}, /* USB Wakeup Event */
+    {EXT_CH_MODE_DISABLED, NULL}, /* RTC Tamper */
+    {EXT_CH_MODE_DISABLED, NULL}, /* RTC Wakeup Event */
+    {EXT_CH_MODE_DISABLED, NULL}, /* COMP1 */
+    {EXT_CH_MODE_DISABLED, NULL}  /* COMP2 */
+  }
+};
+
 /* Application Entry Point */
 int main(void) {
 
     /* System Init */
     halInit();
     chSysInit();
+    
+    /* EXT Driver Init */
+    extStart(&EXTD1, &extcfg);
 
     /* Heartbeat Init */
     chThdCreateStatic(waHBT, sizeof(waHBT), NORMALPRIO, HBT, NULL);
